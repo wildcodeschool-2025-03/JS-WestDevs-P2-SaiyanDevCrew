5aa8339197e784ac43b55459dff6e65d9628e4f1
Luccioni Vincent <vincent.luccioni@gmail.com>
2025-04-18 22:19:03 +0200

    feature/homepage work in progress, creation of CharacterCard

---
+++ client/src/components/CharacterCard/CharacterCard.tsx
@@ -0,0 +1,14 @@
+import "../CharacterCard/CharacterCard.css";
+
+function CharacterCard() {
+  return (
+    <figure>
+      <img
+        src="https://dragonball-api.com/characters/goku_normal.webp"
+        alt=""
+      />
+    </figure>
+  );
+}
+
+export default CharacterCard;
