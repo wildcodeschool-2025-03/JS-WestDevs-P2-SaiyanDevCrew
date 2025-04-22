b382eb3280061af6a17710bffb21408b4b2d747b
Vincent <user@MacBook-Air-de-Vincent.local>
2025-04-22 15:18:46 +0200

    feature/homepage, fetch and map done

--- client/src/pages/homePage/homePage.tsx
+++ client/src/pages/homePage/homePage.tsx
@@ -1,14 +1,39 @@
 import { useEffect, useState } from "react";
 import CharacterCard from "../../components/CharacterCard/CharacterCard";
 import "../homePage/homePage.css";
+import { data } from "react-router";
 
-function homePage() {
-  const [characterData, setCharacterData] = useState ([])
+interface CharacterProps {
+  id: number;
+  image: string;
+  name: string;
+  race: string;
+  gender: string;
+  description: string;
+}
+
+function HomePage() {
+  const [characterData, setCharacterData] = useState<CharacterProps[]>([]);
   useEffect(() => {
     fetch("https://dragonball-api.com/api/characters")
-    .then ((response) => response.json())
-    .then((data)) => setCharacterData(data.items)
-  }, [])
-  return <CharacterCard />;
+      .then((response) => response.json())
+      .then((data) => setCharacterData(data.items));
+  }, []);
+  return (
+    <main className="homepage">
+      {characterData.map((el) => (
+        <CharacterCard
+          key={el.id}
+          id={el.id}
+          image={el.image}
+          name={el.name}
+          race={el.race}
+          gender={el.gender}
+          description={el.description}
+        />
+      ))}
+    </main>
+  );
 }
-export default homePage;
+
+export default HomePage;
