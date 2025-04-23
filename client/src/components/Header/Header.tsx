abfd18a5505739c236b04ec151e3b182e15ef6b2
G-Ludovic <l.galicher@free.fr>
2025-04-23 11:33:28 +0200

    optimized html and css work

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -3,16 +3,16 @@ import { Link } from "react-router";
 
 function Header() {
   return (
-    <header>
-      <figure className="logo">
+    <header className="banner-header">
+      <figure>
         <img src="images/Logo.webp" alt="logo du site" />
       </figure>
-      <figcaption className="title">
+      <figcaption>
         <h1>
           <span>Dragon</span> Fight Simulator
         </h1>
       </figcaption>
-      <nav className="home">
+      <nav>
         <Link to="/home">Home</Link>
       </nav>
     </header>
