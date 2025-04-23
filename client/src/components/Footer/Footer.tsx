6ac5e0534e3be00e62f5fcbd16da0e78d714969b
G-Ludovic <l.galicher@free.fr>
2025-04-23 14:41:28 +0200

    work in progress

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -6,6 +6,7 @@ function Footer() {
       <h2>Le Crew</h2>
       <p>&copy; copyright 2025 - Sayian Crew</p>
       <img src="/images/github-mark.svg" alt="logo GitHub" />
+      <img src="/images/LogoWildCodeSchool.png" alt="logo Wild Code School" />
     </footer>
   );
 }
