f81e57463e6331faf8e2e202d2b89b2e6e2be524
G-Ludovic <l.galicher@free.fr>
2025-04-23 12:27:06 +0200

    create component footer, html and css base

---
+++ client/src/components/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="banner-footer">
+      <h2>Le Crew</h2>
+      <p>&copy; copyright 2025 - Sayian Crew</p>
+      <img src="/images/github-mark.svg" alt="logo GitHub" />
+    </footer>
+  );
+}
+
+export default Footer;
