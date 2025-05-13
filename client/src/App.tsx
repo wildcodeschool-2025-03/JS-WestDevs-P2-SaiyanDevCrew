322422d201b09110e197c94d32b2349884883fb7
mohamed <aouis.mohamed@yahoo.fr>
2025-05-13 23:37:34 +0200

    work in css

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,9 +9,9 @@ function App() {
   return (
     <>
       <Header />
+      <Loginpage/>
       <Outlet />
       <Footer />
-      <Loginpage/>
     </>
   );
 }
--- client/src/pages/Loginpage/Login.tsx
+++ client/src/pages/Loginpage/Login.tsx
@@ -1,13 +1,68 @@
-import { useState } from "react";
+import { type FormEvent, useState } from "react";
+import "./Login.css";
 
-function Loginpage () {
-  const [firstname,setFirstname] = useState('mohamed')
-  
+function Loginpage() {
+  const [name, setname] = useState("");
+  const [pseudo, setpseudo] = useState("");
+  const [email, setemail] = useState("");
+  const [message, setmessage] = useState("");
+
+  const handlesubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    alert(
+      `Name: ${name} Pseudo: ${pseudo} Email: ${email} Message: ${message}`,
+    );
+  };
+
   return (
-    <form>
-      <input type="text" name= "firstname" value={firstname}/>
-    </form>
+    <main className="formule">
+      <section className="form">
+        <img src="/public/images/pngegg_2.png" alt="dragonball" />
+
+        <form onSubmit={handlesubmit}>
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            id="name"
+            value={name}
+            onChange={(e) => setname(e.target.value)}
+          />
+          <label htmlFor="pseudo">Pseudo</label>
+          <input
+            type="pseudo"
+            id="pseudo"
+            value={pseudo}
+            onChange={(e) => setpseudo(e.target.value)}
+          />
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setemail(e.target.value)}
+          />
+          <label htmlFor="message">Message</label>
+          <textarea
+            id="message"
+            name="message"
+            cols={25}
+            rows={2}
+            value={message}
+            onChange={(e) => setmessage(e.target.value)}
+          />
+          <button type="submit">Submit</button>
+          <div className="lien-button">
+            <button className="google" type="button">
+              Google
+            </button>
+            <button className="apple" type="button">
+              Apple
+            </button>
+          </div>
+        </form>
+      </section>
+    </main>
   );
 }
 export default Loginpage;
-   
